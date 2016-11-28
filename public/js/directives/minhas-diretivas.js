8ab8ccabbe52740ab47e827aed8a15a7b2b6a71c
wellington.siqueira <wellingtonex@gmail.com>
2016-11-28 19:26:54 -0300

    separando o template da diretiva em um arquivo .html

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -2,23 +2,11 @@ angular
     .module('minhasDiretivas', [])
     .directive('meuPainel', function() {
         var ddo = {};
-
         ddo.restict = "AE";
-
         ddo.scope = {
             titulo: '@'            
         };
-
         ddo.transclude = true;
-
-        ddo.template = 
-                   '<div class="panel panel-default col-md-2">'
-                   +     '<div class="panel-heading">'
-                   +         '<h3 class="panel-title text-center">{{titulo}}</h3>'
-                   +     '</div>'
-                   +     '<div class="panel-body" ng-transclude>'
-                   +     '</div>'
-                   + '</div>';
-
+        ddo.templateUrl = 'js/directives/meu-painel.html'; 
         return ddo;
     });
