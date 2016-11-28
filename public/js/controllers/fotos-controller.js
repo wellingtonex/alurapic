638d66ff7be7e81d3042ad58679fce86ee3d6336
wellington.siqueira <wellingtonex@gmail.com>
2016-11-28 18:44:24 -0300

    ajustando $http.get e conteudo da view

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,6 +1,18 @@
-angular.module('alurapic').controller('FotosController', function($scope) {
-  $scope.foto = {
-      titulo : 'Leão',
-      url: 'http://www.fundosanimais.com/Minis/leoes.jpg'
-  };
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+
+  $scope.fotos = [];
+
+  // var promisse = $http.get('v1/fotos');
+
+  // promisse.then(function(retorno) {
+  //   $scope.fotos = retorno.data;
+  // }).catch(function(error) {
+  //   console.log(error);    
+  // });
+
+  $http.get('v1/fotos').success(function(fotos) {
+    $scope.fotos = fotos;
+  }).error(function(error) {
+    console.log(error);    
+  });
 });
---
+++ readme.txt
@@ -0,0 +1,2 @@
+O projeto Alurapic utiliza Express para criar endpoints REST e o NeDB, 
+um banco de dados totalmente feito em Node.js e que não necessita qualquer configuração especial.
