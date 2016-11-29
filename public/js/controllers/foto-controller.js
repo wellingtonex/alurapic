3f86d5d4ab0f6991d6bd5f3e1b7b1d0459a5718b
wellington.siqueira <wellingtonex@gmail.com>
2016-11-29 17:12:36 -0300

    cadastrando novas imagens

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,8 +1,19 @@
 angular.module('alurapic').controller('FotoController', function($scope, $http) {
 
     $scope.foto = {};
+    $scope.mensagem = '';
 
     $scope.submeter = function() {
-        console.log($scope.foto);
-    }
+        if($scope.formulario.$valid) {
+            $http.post('v1/fotos', $scope.foto)
+                .success(function() {
+                    $scope.foto = {};
+                    $scope.mensagem = 'Foto incluida com sucesso.';
+                })
+                .error(function(error){
+                    console.log(error);
+                    $scope.mensagem = 'Não foi possivel incluir a foto.';
+                });
+        }
+    };
 });
