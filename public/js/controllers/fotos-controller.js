100cc097e69f9153af467db3e37e838f322a7e02
wellington.siqueira <wellingtonex@gmail.com>
2016-11-29 17:54:45 -0300

    implementando deleção de foto

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -2,6 +2,7 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
 
   $scope.fotos = [];
   $scope.filtro = '';
+  $scope.mensagem = '';
 
   // var promisse = $http.get('v1/fotos');
 
@@ -16,4 +17,17 @@ angular.module('alurapic').controller('FotosController', function($scope, $http)
   }).error(function(error) {
     console.log(error);    
   });
+
+  $scope.remover = function (foto) {
+    $http.delete('v1/fotos/' +foto._id)
+      .success(function() {
+        var indiceFoto = $scope.fotos.indexOf(foto);
+        $scope.fotos.splice(indiceFoto, 1);
+        $scope.mensagem = 'Foto ' + foto.titulo + ' foi removida com sucesso.';
+      })
+      .error(function(error){
+        console.log(error);
+        $scope.mensagem = 'Não foi possivel remover a foto ' + foto.titulo  + '.';
+      }); 
+  };
 });
