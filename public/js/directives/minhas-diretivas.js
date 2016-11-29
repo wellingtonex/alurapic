85a3fe2fe8c5aaa7810463a61cf68316a19efd19
wellington.siqueira <wellingtonex@gmail.com>
2016-11-29 00:12:47 -0300

    criando diretiva minhaFoto

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -9,4 +9,19 @@ angular
         ddo.transclude = true;
         ddo.templateUrl = 'js/directives/meu-painel.html'; 
         return ddo;
+    }).directive('minhaFoto', function() {
+
+        var ddo = {};
+
+        ddo.restrict = "AE";
+
+        ddo.scope = {
+            titulo: '@',
+            url: '@'
+        };
+
+        ddo.template = '<img class="img-responsive center-block" src="{{url}}" alt="{{titulo}}">';           
+
+        return ddo;
+
     });
